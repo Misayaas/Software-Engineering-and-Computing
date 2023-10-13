70a9b1532754ed1d058d7f3533a43168e3100265
Misayaas <113764876+Misayaas@users.noreply.github.com>
2023-10-13 10:56:54 +0800

    26-ztransform

---
+++ ZTransformation.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class ZTransformation
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        String str = input.nextLine();
+        int n = input.nextInt();
+
+        char [][] output = new char[n][str.length()];
+
+        int row=0;
+        int col=0;
+
+        for(char c:str.toCharArray())
+        {
+
+        }
+    }
+}
